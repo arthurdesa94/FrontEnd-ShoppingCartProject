ddbb45e8fe4b41f3b7d3e32d8c9b8481de411da8
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-24 17:56:49 -0300

    Criando pagina carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
 import './App.css';
+import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
       <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/shopping-cart" component={ShoppingCart} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import shoppingCart from '../img/shopping-cart.png';
 
 class Home extends React.Component {
   render() {
 
     return (
       <div>
-        <form>
+        <form className="search-form">
           <input type="text" />
           <button>Buscar</button>
         </form>
+
         <div>
+          <Link to="/shopping-cart" data-testid="shopping-cart-button">
+            <img src={shoppingCart} alt="Shopping Cart Icon" />
+          </Link>
           <p data-testid="home-initial-message">
             Digite algum termo de pesquisa ou escolha uma categoria.
           </p>

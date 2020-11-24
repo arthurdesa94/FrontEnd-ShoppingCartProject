aa966e889f056e5126c9a66ac2c922c1b380687b
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-24 18:42:08 -0300

    Corrindo arquivo App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import ShoppingCart from './components/ShoppingCart';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (

eb9b75d0d1aba5de752fdc01a99e0906deb15229
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-24 18:01:28 -0300

    Adicionando link 'Voltar'

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class ShoppingCart extends React.Component {
   render() {
     return (
       <div>
+        <Link to="/">Voltar</Link>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );

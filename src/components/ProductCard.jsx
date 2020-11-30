fcb520ab14fc35c587ae39725220c6a052da8785
paula-jfe <paula.jfe@gmail.com>
2020-11-30 18:38:28 -0300

    arrumando lint parte 2

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,15 +9,17 @@ class ProductCard extends React.Component {
   }
 
   saveStorage({ target }) {
-    let price = (parseFloat(target.getAttribute('data-price'))).toFixed(2);
+    const oneNegative = -1;
+    const twoPositive = 2;
+    let price = (parseFloat(target.getAttribute('data-price'))).toFixed(twoPositive);
     const id = target.getAttribute('data-id');
     const title = target.getAttribute('data-title');
     const thumbnail = target.getAttribute('data-thumbnail');
     const products = JSON.parse(localStorage.getItem('productsList'));
     const findIndexInArray = products.findIndex((item) => item.id === id);
-    if (findIndexInArray !== -1) {
+    if (findIndexInArray !== oneNegative) {
       products[findIndexInArray].quantity += 1;
-      price = price * products[findIndexInArray].quantity;
+      price *= products[findIndexInArray].quantity;
       products[findIndexInArray].price = price;
       localStorage.setItem('productsList', JSON.stringify([...products]));
     } else {
@@ -36,14 +38,14 @@ class ProductCard extends React.Component {
     return (
       <div>
         <Link
-          to={ `product/${ categoryId }/${ id }` }
+          to={ `product/${categoryId}/${id}` }
           className="link"
           data-testid="product-detail-link"
         >
           <div className="product-card" data-testid="product">
             <h4>{ title }</h4>
             <img alt="Product" src={ thumbnail } />
-            <p>{ `R$ ${ price }` }</p>
+            <p>{ `R$ ${price}` }</p>
           </div>
         </Link>
         <button

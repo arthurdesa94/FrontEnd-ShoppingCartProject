ca395e456f15f21da10e77801cce03b0ec2dce90
Vanessa Oliveira <vanessanaara@gmail.com>
2020-11-30 18:49:11 -0300

    arrumando lint parte 4

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -8,13 +8,13 @@ class ShoppingCart extends React.Component {
     this.increaseproductsList = this.increaseproductsList.bind(this);
     this.decreaseproductsList = this.decreaseproductsList.bind(this);
     this.state = {
-      totalPrice: 0,
+      // totalPrice: 0,
       storage: JSON.parse(localStorage.getItem('productsList')),
-    }
+    };
   }
 
   deleteproductsList({ target }) {
-    let newArray = [];
+    const newArray = [];
     const id = target.getAttribute('data-id');
     const products = JSON.parse(localStorage.getItem('productsList'));
     const findIndexInArray = products.findIndex((item) => item.id === id);
@@ -22,48 +22,61 @@ class ShoppingCart extends React.Component {
       if (findIndexInArray !== index) {
         newArray.push(item);
       }
-    })
+    });
     localStorage.setItem('productsList', JSON.stringify(newArray));
     this.setState({
       storage: newArray,
-    })
+    });
   }
 
   increaseproductsList({ target }) {
     const id = target.getAttribute('data-id');
     const products = JSON.parse(localStorage.getItem('productsList'));
     const findIndexInArray = products.findIndex((item) => item.id === id);
-    let price = ((parseFloat(target.getAttribute('data-price')) / parseInt(target.getAttribute('data-quantity')))).toFixed(2);
+    const two = 2;
+    let price = (
+      (
+        parseFloat(
+          target.getAttribute('data-price')
+        ) / parseInt(target.getAttribute('data-quantity'), 10)
+      )
+    ).toFixed(two);
     products[findIndexInArray].quantity += 1;
-    price = price * products[findIndexInArray].quantity;
+    price *= products[findIndexInArray].quantity;
     products[findIndexInArray].price = price;
     localStorage.setItem('productsList', JSON.stringify(products));
     this.setState({
       storage: JSON.parse(localStorage.getItem('productsList')),
-    })
+    });
   }
 
   decreaseproductsList({ target }) {
     const id = target.getAttribute('data-id');
     const products = JSON.parse(localStorage.getItem('productsList'));
     const findIndexInArray = products.findIndex((item) => item.id === id);
-    let price = ((parseFloat(target.getAttribute('data-price')) / parseInt(target.getAttribute('data-quantity')))).toFixed(2);
+    const two = 2;
+    let price = (
+      (
+        parseFloat(
+          target.getAttribute('data-price')
+        ) / parseInt(target.getAttribute('data-quantity'), 10)
+      )
+    ).toFixed(two);
     products[findIndexInArray].quantity -= 1;
-    price = price * (products[findIndexInArray].quantity);
+    price *= (products[findIndexInArray].quantity);
     products[findIndexInArray].price = price;
     localStorage.setItem('productsList', JSON.stringify(products));
     this.setState({
       storage: JSON.parse(localStorage.getItem('productsList')),
-    })
+    });
   }
 
   // sumValue() {
   // }
 
   render() {
-    const productsList = this.state.storage;
-    /* const { id, title, thumbnail, price, quantity } = productsList; */
-    // const lastItem = productsList.slice(-1).pop();
+    const { storage } = this.state;
+    const productsList = storage;
 
     if (!productsList.length) {
       return (
@@ -79,45 +92,45 @@ class ShoppingCart extends React.Component {
         <Link to="/">Voltar</Link>
         {
           productsList.map((product) => (
-            <div key={`${product.id}`}>
+            <div key={ `${product.id}` }>
               <button
-                data-id={product.id}
-                data-title={product.title}
-                data-thumbnail={product.thumbnail}
-                data-price={product.price}
-                data-quantity={product.quantity}
+                data-id={ product.id }
+                data-title={ product.title }
+                data-thumbnail={ product.thumbnail }
+                data-price={ product.price }
+                data-quantity={ product.quantity }
                 type="button"
-                onClick={this.deleteproductsList}
+                onClick={ this.deleteproductsList }
               >
                 x
-                </button>
-              <img alt="Product" src={product.thumbnail} />
-              <p data-testid="shopping-cart-product-name">{product.title}</p>
+              </button>
+              <img alt="Product" src={ product.thumbnail } />
+              <p data-testid="shopping-cart-product-name">{ product.title }</p>
               <button
-                data-id={product.id}
-                data-title={product.title}
-                data-thumbnail={product.thumbnail}
-                data-price={product.price}
-                data-quantity={product.quantity}
+                data-id={ product.id} 
+                data-title={ product.title } 
+                data-thumbnail={ product.thumbnail }
+                data-price={ product.price }
+                data-quantity={ product.quantity }
                 type="button"
-                onClick={this.decreaseproductsList}
+                onClick={ this.decreaseproductsList }
                 data-testid="product-decrease-quantity"
               >
                 -
-                </button>
-              <p data-testid="shopping-cart-product-quantity">{product.quantity}</p>
+              </button>
+              <p data-testid="shopping-cart-product-quantity">{ product.quantity }</p>
               <button
-                data-id={product.id}
-                data-title={product.title}
-                data-thumbnail={product.thumbnail}
-                data-price={product.price}
-                data-quantity={product.quantity}
+                data-id={ product.id } 
+                data-title={ product.title }
+                data-thumbnail={ product.thumbnail }
+                data-price={ product.price }
+                data-quantity= {product.quantity }
                 type="button"
-                onClick={this.increaseproductsList}
+                onClick={ this.increaseproductsList }
                 data-testid="product-increase-quantity"
               >
                 +
-                </button>
+              </button>
               <p>{product.price}</p>
             </div>
           ))

096fb25462230a76d4cba7435f7c44d3b10db631
paula-jfe <paula.jfe@gmail.com>
2020-12-01 17:53:30 -0300

    Requisito 12 passando

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -44,9 +44,7 @@ class Checkout extends React.Component {
   }
 
   formStates(field, newValue) {
-    this.setState({
-      [field]: newValue 
-    });
+    this.setState({ [field]: newValue });
   }
 
   dataForm() {
@@ -55,33 +53,33 @@ class Checkout extends React.Component {
         <div>
           <p>Informações do comprador</p>
           <label htmlFor="name">Nome completo</label>
-          <input id="name" type="text" placeholder="Digite seu nome completo" data-testid="checkout-fullname" onChange={(event) => this.formStates('name', event.target.value)} />
+          <input id="name" type="text" placeholder="Digite seu nome completo" data-testid="checkout-fullname" onChange={ (event) => this.formStates('name', event.target.value) } />
           <label htmlFor="email">E-mail</label>
-          <input id="email" type="text" placeholder="Digite seu e-mail" data-testid="checkout-email" onChange={(event) => this.formStates('email', event.target.value)} />
+          <input id="email" type="text" placeholder="Digite seu e-mail" data-testid="checkout-email" onChange={ (event) => this.formStates('email', event.target.value) } />
           <label htmlFor="cpf">CPF</label>
-          <input id="cpf" type="text" placeholder="Digite seu CPF" data-testid="checkout-cpf" onChange={(event) => this.formStates('cpf', event.target.value)} />
+          <input id="cpf" type="text" placeholder="Digite seu CPF" data-testid="checkout-cpf" onChange={ (event) => this.formStates('cpf', event.target.value) } />
           <label htmlFor="phone">Telefone</label>
-          <input id="phone" type="text" placeholder="Digite seu telefone" data-testid="checkout-phone" onChange={(event) => this.formStates('phone', event.target.value)} />
+          <input id="phone" type="text" placeholder="Digite seu telefone" data-testid="checkout-phone" onChange={ (event) => this.formStates('phone', event.target.value) } />
           <label htmlFor="cep">CEP</label>
-          <input id="cep" type="text" placeholder="Digite seu CEP" data-testid="checkout-cep" onChange={(event) => this.formStates('cep', event.target.value)} />
+          <input id="cep" type="text" placeholder="Digite seu CEP" data-testid="checkout-cep" onChange={ (event) => this.formStates('cep', event.target.value) } />
           <label htmlFor="address">Endereço</label>
-          <input id="address" type="text" placeholder="Digite seu endereço" data-testid="checkout-address" onChange={(event) => this.formStates('address', event.target.value)} />
+          <input id="address" type="text" placeholder="Digite seu endereço" data-testid="checkout-address" onChange={ (event) => this.formStates('address', event.target.value) } />
         </div>
         <div>
           <p>Método de pagamento</p>
           <label htmlFor="payment">
             <div>
               <p>Boleto</p>
-              <input name="payment" id="boleto" value="boleto" type="radio" onChange={(event) => this.formStates('payment', event.target.value)} />
+              <input name="payment" id="boleto" value="boleto" type="radio" onChange={ (event) => this.formStates('payment', event.target.value) } />
               <img src={ boleto } alt="boleto" width="33px" />
             </div>
             <div>
               <p>Cartão de crédito</p>
-              <input name="payment" value="visa" type="radio" onChange={(event) => this.formStates('payment', event.target.value)} />
+              <input name="payment" value="visa" type="radio" onChange={ (event) => this.formStates('payment', event.target.value) } />
               <img src={ visa } alt="visa" width="40px" />
-              <input name="payment" value="mastercard" type="radio" onChange={(event) => this.formStates('payment', event.target.value)} />
+              <input name="payment" value="mastercard" type="radio" onChange={ (event) => this.formStates('payment', event.target.value) } />
               <img src={ mastercard } alt="mastercard " width="40px" />
-              <input name="payment" value="elo" type="radio" onChange={(event) => this.formStates('payment', event.target.value)} />
+              <input name="payment" value="elo" type="radio" onChange={ (event) => this.formStates('payment', event.target.value) } />
               <img src={ elo } alt="elo" width="40px" />
             </div>
           </label>
@@ -101,25 +99,25 @@ class Checkout extends React.Component {
       <div>
         <div>
           <form>
-            {this.dataForm()}
+            { this.dataForm() }
           </form>
         </div>
         <div>
           <p>Revise seus produtos</p>
-          {storedProducts.map((product => (
-            <div key={`${product.id}`}>
-              <img alt="Product" src={product.thumbnail} />
-              <p data-testid="shopping-cart-product-name">{product.title}</p>
-              <p data-testid="shopping-cart-product-quantity">{product.quantity}</p>
-              <p>{product.price}</p>
+          { storedProducts.map(((product) => (
+            <div key={ `${product.id}` }>
+              <img alt="Product" src={ product.thumbnail } />
+              <p data-testid="shopping-cart-product-name">{ product.title }</p>
+              <p data-testid="shopping-cart-product-quantity">{ product.quantity }</p>
+              <p>{ product.price }</p>
             </div>
-          )))}
+          ))) }
         </div>
 
-        <div>{totalPrice}</div>
+        <div>{ totalPrice }</div>
         <button type="submit" onClick={ this.validateForm }>Comprar</button>
       </div>
-    )
+    );
   }
 }
 
